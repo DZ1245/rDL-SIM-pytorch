58984fb6619af19f6182d7514873b2cddb51f82f
DZ1245 <1369183966@qq.com>
2024-02-26 15:14:55 +0800

    Tep Save

--- src/predict_rDL_Denoising_Module.sh
+++ src/predict_rDL_Denoising_Module.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES='2' CUDA_DEVICE_ORDER=PCI_BUS_ID \
+CUDA_VISIBLE_DEVICES='0,1,2' CUDA_DEVICE_ORDER=PCI_BUS_ID \
 python  -m torch.distributed.launch --nproc_per_node 1 \
                                     predict_rDL_Denoising_Module.py \
                                     --root_path /mnt/data2_16T/datasets/zhi.deng/Biology_cell/rDL_SIM/SR/ \
