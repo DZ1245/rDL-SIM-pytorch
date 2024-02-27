a99ca0dfa411e37fbd61b0fa871171fe2e773fc7
DZ1245 <1369183966@qq.com>
2024-02-27 19:05:09 +0800

    20240227

--- README.md
+++ README.md
@@ -16,12 +16,16 @@
 
 ## Questions
 - 为什么DN_model的input_channels是nphases=3，因为代码将9通道图片分为3*3进行训练，不明白原因
-- 论文中，SR模型在DN中不进行训练，但是代码中仍然存在SR的optimizer_sr，先在DN中不训练SR
+- ~~论文中，SR模型在DN中不进行训练，但是代码中仍然存在SR的optimizer_sr，先在DN中不训练SR~~
 - ~~TF代码中的ReduceLROnPlateau，需要在看看~~
-- 直接采用pytorchi中的学习率优化参数
-- rDL-DN的predict中num_average的作用未知
-- 原TF代码中，训练中cur_k0和modamp通过img_gt计算得到，然后在计算得到img_gen时使用了这些参数，存在问题cur_k0包含有gt信息？
+- 直接采用pytorch中的学习率优化参数
+- rDL-DN的predict中num_average的作用未知：获取光照模式??
+- 原TF代码中，训练中cur_k0和modamp通过img_gt计算得到，然后在计算得到img_gen时使用了这些参数，存在问题cur_k0包含有gt信息?
+
+## Datasets
+- rDL_SIM_data/rDL_BioSR/rDL_SIM：train和val在细胞级划分，SR和DN均包括了所有数据
+- rDL_SIM_data/rDL_BioSR/rDL_SIM_separate_v1：train和val在细胞级划分，SR和DN在切割好的图片中随机划分
 
 ## Log
 - 2024.02.26:创建Test分支，开始尝试修改代码及过往疑问;处理pku数据，暂时只能划分9x9x55张128x128图片（论文说它划分了3w张），并根据论文划分数据集（SR三分之一，rDL三分之二）；**pku数据无法读取，rDL提供的读取代码不适用，尚未解决,暂时继续使用BioSR数据**
-- 
+- 2024.02.27:重新按比例划分BioSR数据;**疑问：不同光照强度的相同图片被分开到train和val会影响吗**；
--- src/train_SR_Inference_Module.sh
+++ src/train_SR_Inference_Module.sh
@@ -1,19 +1,19 @@
 CUDA_VISIBLE_DEVICES='0,1,2,3' CUDA_DEVICE_ORDER=PCI_BUS_ID \
 python -m torch.distributed.launch  --nproc_per_node 4 \
                                     train_SR_Inference_Module.py \
-                                    --root_path /mnt/data2_16T/datasets/zhi.deng/Biology_cell/rDL_SIM_data/rDL_SIM/SR/ \
-                                    --data_folder DL1to1downsample \
-                                    --dataset DL1to1downsample \
+                                    --root_path /mnt/data2_16T/datasets/zhi.deng/Biology_cell/rDL_SIM_data/rDL_BioSR/rDL_SIM_separate_v1/SR \
+                                    --data_folder Microtubules \
+                                    --dataset Microtubules \
                                     --input_height 128 --input_width 128 \
-                                    --input_channels 1 --out_channels 1 --scale_factor 2 \
+                                    --input_channels 9 --out_channels 1 --scale_factor 2 \
                                     --model_name DFCAN \
                                     --norm_flag 1 --total_epoch 10000 --sample_epoch 2 \
                                     --log_iter 10 --num_workers 30 \
                                     --batch_size 28 --start_lr 1e-4 --lr_decay_factor 0.5 \
                                     --ssim_weight 1e-1 \
-                                    --load_weights_flag 1 \
-                                    --exp_name DL1to1downsample_finetuneMT \
-                                    --resume_name DL1to1downsample_finetuneMT \
+                                    --load_weights_flag 0 \
+                                    --exp_name BioSR_separate \
+                                    --resume_name BioSR_separate \ 
 
                                         
 # 4090 DFCAN batchsize = 28 DFCAN_SimAM batchsize = 21
